f684aa4e960f27cf54b43ee11c6c7a623f610a25
fenelon01 <90154846+fenelon01@users.noreply.github.com>
2024-07-29 15:09:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social Media Webpage</title>
+    <link rel="stylesheet" href="/PHP PROJECT/style.css">
+</head>
+<body>
+    <div class="hero">
+        <nav>
+            <img src="images/logo6.png" class="logo">
+            <ul>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">How it works</a></li>
+                <li><a href="#">Privacy</a></li>
+            </ul>
+            <div>
+                <a href="#" class="login-btn">Login</a>
+                <a href="#" class=" btn">Download app</a>
+            </div>
+        </nav>
+        <div class="content">
+            <h1>Make<br>New Friends</h1>
+            <p> Camdom Its a social web where you can meet people from the Dominican Repluc - Haiti and Miami.</p>
+            <a href="#" class="btn">Join Now</a>
+
+
+        </div>
+    </div>
+</body>
+</html>
+
